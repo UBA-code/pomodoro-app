e5e4d1ca64dca569c5ad69e75be24a982fa9857a
UBA-code <belhachmiyassine4@gmail.com>
2024-01-20 21:23:51 +0100

    initial commit

---
+++ public/script/main.js
@@ -0,0 +1,78 @@
+"use strict";
+// const progress = document.querySelector(".progress-ring__circle") as SVGCircleElement;
+// const circumference: number = progress.r.baseVal.value * 2 * Math.PI;
+// progress.style.strokeDasharray = `${circumference} ${circumference}`;
+// progress.style.strokeDashoffset = circumference.toString();
+// function setProgress(percent: number) {
+//     const offset: string = (circumference - percent / 100 * circumference).toString();
+//     progress.style.strokeDashoffset = offset;
+// }
+// setProgress(-25);
+var Sessions;
+(function (Sessions) {
+    Sessions[Sessions["pomodoro"] = 0] = "pomodoro";
+    Sessions[Sessions["short"] = 1] = "short";
+    Sessions[Sessions["long"] = 2] = "long";
+})(Sessions || (Sessions = {}));
+let mainObj = JSON.parse(localStorage.getItem('data'));
+if (!mainObj) {
+    mainObj = { colorTheme: "red", totalTime: "00:00:00", session: Sessions.pomodoro };
+    localStorage.setItem('data', JSON.stringify(mainObj));
+}
+let sessionsOptions = document.querySelectorAll('.selecter .option');
+sessionsOptions.forEach((elem, i) => {
+    elem.addEventListener('click', (e) => {
+        sessionsOptions.forEach(e => e.classList.remove('active-option'));
+        elem.classList.add('active-option');
+        updateData(Object.assign(Object.assign({}, mainObj), { session: i }));
+    });
+    if (i == mainObj.session)
+        elem.classList.add('active-option');
+});
+function updateData(obj) {
+    mainObj = obj;
+    localStorage.setItem('data', JSON.stringify(obj));
+}
+let counter = document.querySelector('.counter');
+let startResetBtn = document.querySelector('.start-reset');
+let pauseResumeBtn = document.querySelector('.pause-resume');
+let time = new Date("2000 00:25:00");
+let appLoop;
+startResetBtn.addEventListener('click', switchStartReset);
+pauseResumeBtn.addEventListener('click', runCounter);
+/**
+ * switch status between START & RESET
+*/
+function switchStartReset() {
+    if (counter.classList.contains('counter-active')) //? counter is already running
+        resetCounter();
+    else
+        runCounter();
+}
+function runCounter() {
+    startResetBtn.textContent = "RESET";
+    pauseResumeBtn.textContent = "PAUSE";
+    counter.classList.toggle('counter-active'); // toggle between pause and resume
+    if (counter.classList.contains('counter-active')) {
+        appLoop = setInterval(() => {
+            time.setSeconds(time.getSeconds() - 1);
+            counter.textContent = time.toTimeString().match(/\d{2}:\d{2} /ig)[0];
+            if (counter.textContent.match('25:00'))
+                clearInterval(appLoop);
+        }, 1000);
+        startResetBtn.addEventListener('click', resetCounter);
+    }
+    else {
+        pauseResumeBtn.textContent = "RESUME";
+        clearInterval(appLoop);
+    }
+}
+function resetCounter() {
+    counter.textContent = '25:00';
+    startResetBtn.textContent = "START";
+    pauseResumeBtn.textContent = "";
+    counter.classList.remove('counter-active');
+    clearInterval(appLoop);
+    time = new Date("2000 00:25:00");
+    startResetBtn.removeEventListener('click', resetCounter);
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'html': '#1E213E',
+        'white-text': '#D6DFFF',
+        'dark-blue': '#161932',
+        'light-blue': '#72F4F8',
+      },
+    },
+  },
+  plugins: [],
+}
+
